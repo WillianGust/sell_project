bdd06c0aec5f78dadcb421c55bebd912becbf4d7
WillianGust <williandes.silva@yahoo.ca>
2022-06-06 16:57:53 -0400

    Produtos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :fornecedores
   resources :produtos
   get 'home/index'
   resources :clientes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_202927) do
+ActiveRecord::Schema.define(version: 2022_06_06_204700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2022_06_06_202927) do
     t.string "nome"
     t.string "email"
     t.string "telefone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fornecedores", force: :cascade do |t|
+    t.string "nome"
+    t.string "telefone"
+    t.string "email"
+    t.string "site"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
